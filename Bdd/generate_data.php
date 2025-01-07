70e2631881182be129d4eb7accf8dca1298ae2e4
kingaventure <j.groult45@gmail.com>
2025-01-07 15:37:11 +0100

    ajout de l'autocompletion des données

--- Bdd/generate_data.php
+++ Bdd/generate_data.php
@@ -1,21 +1,60 @@
 <?php
 require_once 'vendor/autoload.php';
 
-$pdo = new PDO('mysql:host=localhost;dbname=ma_base', 'root');
+$pdo = new PDO('mysql:host=localhost;dbname=ecommerce_julien', 'root');
 $faker = Faker\Factory::create();
 
-for ($i = 0; $i < 10; $i++) {
-    $nom = $faker->name();
+for ($i = 0; $i < 100; $i++) {
+    $username = $faker->userName();
     $email = $faker->email();
-    $adresse = $faker->address();
+    $password = $faker->password();
+    $disabled = $faker->numberBetween(0, 1);
 
-    $query = $pdo->prepare("INSERT INTO user (nom, email, adresse) VALUES (:nom, :email, :adresse)");
+    $name = $faker->word();
+    $description = $faker->paragraph();
+    $category = $faker->randomElement(['télevision', 'papetrie', 'Informatique', 'Mobilier', 'avion']);
+    $image = "https://cds.thalesgroup.com/sites/default/files/2023-12/csm_16920180628_fuego_thinkstock_489587e013.png";
+    $prix = $faker->randomNumber();
+    $stock = $faker->randomNumber();
+
+
+    $query = $pdo->prepare("INSERT INTO user (Username, Password, Email, Disabled) VALUES (:Username, :Password, :Email, :Disabled)");
     $query->execute([
-        'nom' => $nom,
-        'email' => $email,
-        'adresse' => $adresse,
+        'Username' => $username,
+        'Email' => $email,
+        'Password' => $password,
+        'Disabled' => $disabled,
     ]);
+
+    $query = $pdo->prepare("INSERT INTO article (Name, Description, Category, Image, Prix, Stock) VALUES (:Name, :Description, :Category, :Image, :Prix, :Stock)");
+    $query->execute([
+        'Name' => $name,
+        'Description' => $description,
+        'Category' => $category,
+        'Image' => $image,
+        'Prix' => $prix,
+        'Stock' => $stock,
+    ]);
+
+
 }
 
-echo "10 utilisateurs insérés avec succès !\n";
+for ($i = 0; $i < 5; $i++) {
+    $query = $pdo->prepare("INSERT INTO category (Name) VALUES (:Name)");
+        if($i === 0){
+            $value = "télevision";
+        } else if ($i === 1){
+            $value = "papetrie";
+        } else if ($i === 2){
+            $value = "Informatique";
+        } else if ($i === 3){
+            $value = "Mobilier";
+        } else if ($i === 4){
+            $value = "avion";
+        }
+
+        $query->execute([
+            'Name' => $value,
+        ]);
+    }
 ?>
