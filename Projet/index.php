3a0974884beb9c34ffba418553ad3ccb8af737cd
kingaventure <j.groult45@gmail.com>
2025-01-30 13:25:58 +0100

    Premier push de vérification

--- Projet/index.php
+++ Projet/index.php
@@ -43,7 +43,7 @@
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css"
         rel="stylesheet"
     >
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="./includes/bootstrap/bootstrap.min.css" rel="stylesheet">
     <link rel="icon" href="../Documentation/logo.ico">
     <title>Projet de julien</title>
 </head>
@@ -63,7 +63,7 @@
         }
     ?>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="./includes/bootstrap/bootstrap.min.js"></script>
 <script src="./asset/js/Services/cart.js"></script>
 </body>
 </html>
